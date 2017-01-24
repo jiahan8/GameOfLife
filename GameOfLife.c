a451e01d85ac2610b33be94167fdca0875835160
JiaHan <jiahantan96@gmail.com>
2017-01-23 23:49:06 -0600

    Update GameOfLife.c

--- GameOfLife.c
+++ GameOfLife.c
@@ -32,157 +32,12 @@
    // 2 1 2 3 3 4 4 4 5 1 5 4 6 2 6 3 6 4 15 0 16 1 14 2 15 2 16 2
    // 812 steady state
    //1 5 2 5 1 6 2 6 11 5 11 6 11 7 12 4 12 8 13 3 13 9 14 3 14 9 15 6 16 4 16 8 17 5 17 6 17 7 18 6 21 3 21 4 21 5 22 3 22 4 22 5 23 2 23 6 25 1 25 2 25 6 25 7 35 3 35 4 36 3 36 4		
-//		System.out.println("copy");
-//		for(int row =  0; row < cell.length   ; row++){
-//			for(int col = 0; col < cell[row].length   ; col++ ){
-//
-//				checkNeighbors(cell);
-//				
-//				cells[row][col]=cell[row][col];
-//			}
-//		}
-//		System.out.println("copy complete");
+
 		
-		
-		
-//		checkNeighbors(cells);
-//		for(int row =  0; row < cells.length   ; row++){
-//			for(int col = 0; col < cells[row].length   ; col++ ){
-//
-//				
-////				checkNeighbors(cells);
-//
-//				if(cells[row][col] == true)
-//					System.out.print("O");
-//				else if(cells[row][col] == false)
-//					System.out.print(".");
-//				else
-//					System.out.print("V");
-//			}
-//			System.out.println();
-//		}
-//		checkNeighbors(cells);
-//		
-//		for(int row =  0; row < cell.length   ; row++){
-//			for(int col = 0; col < cell[row].length   ; col++ ){
-//				cell[row][col]=cells[row][col];
-//			}
-//		}
-		
-//		showAllCells(cell);
-//		cell=cells;
-		//		System.out.println();
 	}
  int checkNeighbors(int row, int col,  bool cell[24][80]){
    int alive = 0;
-   //int dead = 0;
-   //bool cells[24][80];
-   //		int row=0;
-   //		int col=0;
-   //		for(int row =  0   ; row < 24   ; row++){
-   //			for(int col = 0   ; col < 80   ; col++ ){
-   //				if(cells[row][col]=true){ // current cell is alive
-   //				if(cell[row][col]=true){ // current cell is alive
-   ////					
-   //					if(cell[row-1][col-1]==false){ // top left
-   //						dead++;
-   //					}
-   //					else if(cell[row-1][col-1]==true){ // top left
-   //						alive++;
-   //					}
-   //					else{
-   //						
-   //					}
-   //						
-   //					if(cell[row-1][col ]==false) // top
-   //						dead++;
-   //					else if(cell[row-1][col]==true) // top
-   //						alive++;
-   //					else{
-   //						
-   //					}
-   //						
-   //					if(cell[row-1][col+1]==false) // top right
-   //						dead++;
-   //					else if(cell[row-1][col+1]==false) // top right
-   //						alive++;
-   //					else{
-   //						
-   //					}
-   //					if(cell[row][col-1]==false) // left
-   //						dead++;
-   //					else if(cell[row][col-1]==false) // left
-   //						alive++;
-   //					else{
-   //						
-   //					}
-   //					if(cell[row][col+1]==false) // right
-   //						dead++;
-   //					else if(cell[row][col+1]==false) // right
-   //						alive++;
-   //					else{
-   //						
-   //					}
-   //					if(cell[row+1][col-1]==false) // bottom right
-   //						dead++;
-   //					else if(cell[row+1][col-1]==false) // bottom right
-   //						alive++;
-   //					else{
-   //						
-   //					}
-   //					if(cell[row+1][col]==false) // bottom
-   //						dead++;
-   //					else if(cell[row+1][col]==false) // bottom
-   //						alive++;
-   //					else{
-   //						
-   //					}
-   //					if(cell[row+1][col+1]==false) // bottom left					
-   //						dead++;
-   //					else if(cell[row+1][col+1]==false) // bottom left					
-   //						alive++;
-   //					else{
-   //						
-   //					}
-   //				}
-   //				else if(cell[row][col]=false){
-   //					if(cell[row-1][col-1]==false) // top left
-   //						dead++;
-   //					else
-   //						alive++;
-   //					if(cell[row-1][col]==false) // top
-   //						dead++;
-   //					else
-   //						alive++;
-   //					if(cell[row-1][col+1]==false) // top right
-   //						dead++;
-   //					else
-   //						alive++;
-   //					if(cell[row][col-1]==false) // left
-   //						dead++;
-   //					else
-   //						alive++;
-   //					if(cell[row][col+1]==false) // right
-   //						dead++;
-   //					else
-   //						alive++;
-   //					if(cell[row+1][col-1]==false) // bottom right
-   //						dead++;
-   //					else
-   //						alive++;
-   //					if(cell[row+1][col]==false) // bottom
-   //						dead++;
-   //					else
-   //						alive++;
-   //					if(cell[row+1][col+1]==false) // bottom left					
-   //						dead++;
-   //					else
-   //						alive++;
-   //				}
-   //				else{
-   //					
-   //				}
-		
+   
 		
    if (cell[ (row -1+24)%24 ][(col - 1+80)%80] == true) {
      alive++;
@@ -217,161 +72,14 @@
      //	        System.out.println("yeahyeah");
    }
 			
-		
-		
-   //				if (row > 0 && col > 0 && cell[row - 1][col - 1] == true) {
-   //			        alive++;
-   //			    }
-   //			    ;
-   //			    if (row > 0 && cell[row - 1][col] == true) {
-   //			        alive++;
-   //			    }
-   //			    ;
-   //			    if (row > 0 && col < 72 && cell[row - 1][col + 1] == true) {
-   //			        alive++;
-   //			    }
-   //			    ;
-   //			    if (col > 0 && cell[row][col - 1] == true) {
-   //			        alive++;
-   //			    }
-   //			    ;
-   //			    if (col < 72 && cell[row][col + 1] == true) {
-   //			        alive++;
-   //			    }
-   //			    ;
-   //			    if (col > 0 && row < 22 && cell[row + 1][col - 1] == true) {
-   //			        alive++;
-   //			    }
-   //			    ;
-   //			    if (row < 22 && cell[row + 1][col] == true) {
-   //			        alive++;
-   //			    }
-   //			    ;
-   //			    if (row < 22 && col < 72 && cell[row + 1][col + 1] == true) {
-   //			        alive++;
-   ////			        System.out.println("yeahyeah");
-   //			    }
-		
-		
-		
-   //		 int count = 0;
-   //		    for(int i = row - 1; i <= row + 1; i++){
-   //		        if (i >= 0 && i < cell.length){ // fixed here
-   //		        	for(int j = col - 1; j <= col + 1; j++){
-   //		        		if (j >= 0 && j < cell[i].length){ // fixed here
-   //		        			if (i != row || j != col){
-   //		        				if (cell[i][j] == true) 
-   //		        					count++;
-   //		        			}
-   //		        		}
-   //		        	} // end for
-   //		        }
-   //		    } // end for
-   //
-   //		    return count;
-		    
-		
-		
-   //		    int numLiveNeighbors = 0; 
-   //			for(int x = row - 1; x < row + 2; x++){
-   //				for(int y = col - 1; y < col + 2; y++){
-   //					if(x == row && y == col) continue;
-   //					numLiveNeighbors += isAlive(x,y);
-   //				}
-   //			}
-   //			return numLiveNeighbors;
-		    
-		
-   //		    int neighbours = 0;
-   //		    for (int xOffset = -1; xOffset < 2; xOffset++) {
-   //		        for (int yOffset = -1; yOffset < 2; yOffset++) {
-   //		            if ((xOffset != 0 || yOffset != 0)
-   //		                  && board.get(row + yOffset, col + xOffset)) {
-   //		                neighbours++;
-   //		            }
-   //		        }
-   //		    }
-   //		    return neighbours;
-		    
-		    
-   //		        int cnt = 0;
-   //		        for (int[] offset : NEIGHBOURS) {
-   //		            if (cells.getTile(x + offset[1], y + offset[0]) {
-   //		               cnt++;
-   //		            }
-   //		        }
-   //		        return cnt;
-
-		        
-			    
-   //				for( int row =  0; row < 24 ; row++){
-   //					for( int col = 0; col < 80 ; col++ ){
-   //						if(cell[row][col]==true){ // if current cell is alive
-   //							if(alive > 3 || alive < 2){
-   //								cell[row][col]=false;
-   ////								return false;
-   //							}
-   //							else{
-   //								cell[row][row]=true;
-   //							}
-   //						}
-   //						else if(cell[row][col]==false){ // if current cell is not alive
-   //							if(alive == 3){
-   //								cell[row][col]=true;
-   //							}
-   //							else{
-   //								cell[row][row]=false;
-   //							}
-   //						}
-   //						else{
-   //							
-   //						}
-   //					}
-   //				}
+		row + 1][col + 1] == true) {
+   sAlive(x,y);
+			
+			
+			
+			
    return alive;  
 			    
-			
-   //		}// end double for loop
-   //		return false;
-		
-   //count alive/dead neighbors
-   //		for( int row =  0; row < 24 ; row++){
-   //			for( int col = 0; col < 80 ; col++ ){
-   //				if(cell[row][col]==true){ // if current cell is alive
-   //					if(alive > 3 || alive < 2){
-   //						cell[row][col]=false;
-   ////						return false;
-   //					}
-   //					else{
-   //						cell[row][row]=true;
-   //					}
-   //				}
-   //				else if(cell[row][col]==false){ // if current cell is not alive
-   //					if(alive == 3){
-   //						cell[row][col]=true;
-   //					}
-   //					else{
-   //						cell[row][row]=false;
-   //					}
-   //				}
-   //				else{
-   //					
-   //				}
-   //			}
-   //		}
-   //		return false;
-		
-   //		return cells[row][col];
-		
-   //		for(int row =  0; row < 24 ; row++){
-   //			for(int col = 0; col < 80 ; col++ ){
-   //
-   //				}
-   //			}
-   //		}
-		
-   //		showAllCells(cell);
-		
 	
 
 }
@@ -419,51 +127,6 @@ for( row =  0; row <  ROWARRAY   ; row++){
 		  cells[y][x]=true;
 		}
 
-
-		//for(  row =  0; row < 24 ; row++){
-		//for(  col = 0; col < 80 ; col++ ){
-	    //				cells[0][1] = true;
-	    //				cells[6][8] = true;
-	    //				cells[9][9] = true;
-	    //				cells[9][10]= true;
-				
-	    // 371 collides each other
-	    // 463 steady state
-		//cells[1][2]=true;
-		//cells[3][2]=true;
-		//cells[4][3]=true;
-		//cells[4][4]=true;
-		//cells[1][5]=true;
-		//cells[4][5]=true;
-		//cells[2][6]=true;
-		//cells[3][6]=true;
-		//cells[4][6]=true;
-		//cells[0][15]=true;
-		//cells[1][16]=true;
-		//cells[2][14]=true;
-		//cells[2][15]=true;
-		//cells[2][16]=true;
-
-	    // testing
-	    //				cells[6][6]=true;
-	    //				cells[7][6]=true;
-	    //				cells[8][6]=true;
-	    //				cells[9][6]=true;
-	    //				cells[6][7]=true;
-	    //				cells[7][7]=true;
-	    //				cells[8][7]=true;
-	    //				cells[9][7]=true;
-	    //				cells[6][8]=true;
-	    //				cells[7][8]=true;
-	    //				cells[8][8]=true;
-	    //				cells[9][8]=true;
-	    //				{{2, 1},
-	    //				{2, 3},
-	    //				{3, 4}, {4, 4}, {5, 1}, {5, 4}, {6, 2}, {6, 3} ,{6, 4}, {15, 0} ,
-	    //				{16, 1}, {14, 2}, {15, 2}, {16, 2}};
-
-		//}
-		//}
 		
 	// show all cells every round
 	int round=1;
@@ -517,23 +180,9 @@ for( row =  0; row <  ROWARRAY   ; row++){
 		else{
 		  newCells[row2][col2]=true;
 		}
-									
-		//									if(alivenum == 2 || alivenum == 3){
-		//										newCells[row][col]=true;
-		//									}
-		//									else {
-		//										newCells[row][col]=false;
-		//									}
-									
-		//									if(alivenum > 3 || alivenum < 2){
-		//										newCells[row][col]=false;
-		////										return false;
-		//									}
-		//									else if(alivenum==2 || alivenum==3)
-		//										newCells[row][col]=true;
-		//									else{
-		//										newCells[row][col]=true;
-		//									}
+	
+		      
+		      
 	      }
 	      else if(currentCells[row2][col2]==false){ // if current cell is not alive
 		if(alivenum == 3){
@@ -544,12 +193,8 @@ for( row =  0; row <  ROWARRAY   ; row++){
 		}
 	      }
 	      else{
-		//									if(alivenum == 3){
-		//										newCells[row][col]=true;
-		//									}
-		//									else{
-		//										newCells[row][col]=false;
-		//									}
+	
+		      
 	      }
 	      //							cells[row][col]=currentCells[row][col];						
 
