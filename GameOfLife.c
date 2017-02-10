12d100a485dc150c4fdc9aa56f51ffdccd3dcd5f
JiaHan <jiahantan96@gmail.com>
2017-02-09 21:59:05 -0600

    Update GameOfLife.c

--- GameOfLife.c
+++ GameOfLife.c
@@ -9,11 +9,7 @@
 #define COLARRAY 80
 
  void showAllCells(bool cell[24][80]){
-//		checkNeighbors(cell);
-   //bool cells[24][80];
-//		boolean cell = false;
-//		int row=0;
-//		int col=0;
+
    int row;
    int col;
    for( row =  0; row <  24   ; row++){
@@ -32,8 +28,6 @@
    // 2 1 2 3 3 4 4 4 5 1 5 4 6 2 6 3 6 4 15 0 16 1 14 2 15 2 16 2
    // 812 steady state
    //1 5 2 5 1 6 2 6 11 5 11 6 11 7 12 4 12 8 13 3 13 9 14 3 14 9 15 6 16 4 16 8 17 5 17 6 17 7 18 6 21 3 21 4 21 5 22 3 22 4 22 5 23 2 23 6 25 1 25 2 25 6 25 7 35 3 35 4 36 3 36 4		
-
-		
 	}
  int checkNeighbors(int row, int col,  bool cell[24][80]){
    int alive = 0;
@@ -85,31 +79,13 @@
 }
 
 
-
-
-
-
-
-
-
-
 int main (int argc, char *argv[]){
 
-        //boolean [][] cells = new boolean [24][80] ;
-	//boolean [][] currentCells= new boolean[24][80];
-       	//boolean [][] newCells = new boolean [24][80] ;
-  //bool xxx[rowarray][colarray];
 
   bool cells[24][80];
     bool currentCells[24][80];
   bool newCells[24][80];
 
-//		int alivenum=0;
-		
-//		boolean [] singlecell = false;
-//		boolean[] cells = new boolean[80];
-		
-		// read alive cells
 		int row;
 		int col;
 
@@ -130,27 +106,14 @@ for( row =  0; row <  ROWARRAY   ; row++){
 		
 	// show all cells every round
 	int round=1;
-	//bool currentCell = false;
-	//int i;
-	//		for(int row =  0; row < cells.length ; row++){
-	//			for(int col = 0; col < cells[row].length ; col++ ){
-	//		while(cells[row][col]==true){
 	
 
-//for( i = 0; i< 600 ; i++){
 	while(1){			
 	  //				currentCells[row][col]=false;
 	  printf("%d\n", round);
-	  //				System.out.println(cells.length);
-	  //				System.out.println(cells[row].length);
-				
-	  //					System.out.println(round);
+	
 	  showAllCells(cells);
-	  //			currentCells[row][col] = cells[row][col];
-	  //					System.out.println(cells[2][6]+" oriori");
-	  //					System.out.println(cells[3][6]+" oriori");
-	  //					System.out.println(cells[3][5]+" oriori 2");
-	  //					System.out.println(cells[3][7]+" oriori 2");
+
 
 	  int row1;
 	  int col1;			
@@ -160,11 +123,7 @@ for( row =  0; row <  ROWARRAY   ; row++){
 	      //	cells[row][col]=currentCells[row][col];		
 	    }
 	  }
-	  //					System.out.println(currentCells[2][6]+" oriori");
-	  //					System.out.println(currentCells[3][6]+" oriori");
-	  //					System.out.println(currentCells[3][5]+" oriori 2");
-	  //					System.out.println(currentCells[3][7]+" oriori 2");
-
+	
 	  int row2;
 	  int col2;
 	  for( row2 =  0; row2 < 24 ; row2++){
@@ -172,7 +131,6 @@ for( row =  0; row <  ROWARRAY   ; row++){
 	      int alivenum=0;
 	      alivenum = checkNeighbors(row2, col2, currentCells);	
 							 
-	      //							 System.out.println(alivenum);
 	      if(currentCells[row2][col2]==true){ // if current cell is alive
 		if(alivenum < 2 || alivenum > 3){
 		  newCells[row2][col2]=false;
@@ -180,8 +138,6 @@ for( row =  0; row <  ROWARRAY   ; row++){
 		else{
 		  newCells[row2][col2]=true;
 		}
-	
-		      
 		      
 	      }
 	      else if(currentCells[row2][col2]==false){ // if current cell is not alive
@@ -216,24 +172,13 @@ for( row =  0; row <  ROWARRAY   ; row++){
 	  //					cells[10][20]=true;
 					
 	  printf("\n");
-	  //					System.out.println(cells[2][6]+" oriori");
-	  //					System.out.println(cells[3][6]+" oriori");
-	  //					System.out.println(cells[3][5]+" oriori 2");
-	  //					System.out.println(cells[3][7]+" oriori 2");
-					
-	  //					checkNeighbors(currentCells);
-	  //					cells[row][col]=checkNeighbors(currentCells);
-	  //					cells=currentCells;
 					
 	  //			cells[row][col] = currentCells[row][col];
 	  round++;
 	  //				}
 	  //				round++;
 	  printf("\n");
-	  //				System.out.println();
-				
-	  //			}
-
+	
 
 	  
 	  usleep(83333);
